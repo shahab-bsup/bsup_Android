00f6bc8d683dd6b1dce037ffa7a1012880a6d114
shahabshahab <shahabgolpaiegan@gmail.com>
2018-06-08 18:11:28 +0430

    ...

---
+++ app/src/main/java/tk/medlynk/patient/android/ViewModel/MedlynkViewModel.java
@@ -0,0 +1,49 @@
+package tk.medlynk.patient.android.ViewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+import tk.medlynk.patient.android.DataBase.DataBaseModel;
+import tk.medlynk.patient.android.Repository.MedlynkRepository;
+
+/**
+ * Created by Shahab on 6/6/2018.
+ */
+
+public class MedlynkViewModel extends AndroidViewModel {
+
+    private String name ;
+
+    private MedlynkRepository mRepository;
+    private LiveData<DataBaseModel> mGetAnswers;
+
+    public MedlynkViewModel(Application application) {
+        super ( application );
+        mRepository = new MedlynkRepository ( application );
+    }
+
+    public LiveData<DataBaseModel> getAnswers(int appointmentId, int tableNumber, int questionNumber) {
+        mGetAnswers = mRepository.GetAnswerRecord ( appointmentId, tableNumber, questionNumber );
+        return mGetAnswers;
+    }
+
+    public void insertAnswersToDB(int appointmentId, int tableNumber, int questionNumber, String answersJson) {
+        mRepository.InsertAnswerRecord ( appointmentId, tableNumber, questionNumber, answersJson );
+    }
+
+    public void updateAnswersToDB(int appointmentId, int tableNumber, int questionNumber, String answersJson) {
+        mRepository.UpdateAnswerRecord ( appointmentId, tableNumber, questionNumber, answersJson );
+    }
+
+    public void changeName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
